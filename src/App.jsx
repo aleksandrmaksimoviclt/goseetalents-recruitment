870cda4a3c389c73787ddaa4293fd060d7d2a69c
Aleksandr Maksimovic <aleksandr.maksimoviclt@gmail.com>
2017-10-24 16:26:32 +0200

    Add static search-field component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
+import './fonts/font-awesome/css/font-awesome.css';
+
 
 import Header from './components/header/header';
+import SearchField from './components/search-field/search-field';
 
 class App extends Component {
   render() {
     return (
-      <Header />
+      <div className="App">
+        <Header />
+        <SearchField />
+      </div>
     );
   }
 }
