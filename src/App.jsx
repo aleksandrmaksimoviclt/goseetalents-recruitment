5ec80fcdb3216f76f0544a3f38e6303877b2bfcf
Aleksandr Maksimovic <aleksandr.maksimoviclt@gmail.com>
2017-10-24 13:01:23 +0200

    Rename to jsx

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import Header from './components/header/header';
+
+class App extends Component {
+  render() {
+    return (
+      <Header />
+    );
+  }
+}
+
+export default App;
